324179d11219401e4504715ffb886ef4dd82ee89
Wojciech Stelmaszewski <wojciech.stelmaszewski@aldautomotive.com>
2018-01-16 11:11:05 +0000

    MicrosoftBotFrameworkFaq add.

--- WeatherBot/Controllers/MessagesController.cs
+++ WeatherBot/Controllers/MessagesController.cs
@@ -1,9 +1,11 @@
-﻿using System.Net;
+﻿using System;
+using System.Net;
 using System.Net.Http;
 using System.Threading.Tasks;
 using System.Web.Http;
 using Microsoft.Bot.Builder.Dialogs;
 using Microsoft.Bot.Connector;
+using WeatherBot.Faq;
 
 namespace WeatherBot
 {
@@ -18,43 +20,13 @@ namespace WeatherBot
         {
             if (activity.Type == ActivityTypes.Message)
             {
-                await Conversation.SendAsync(activity, () => new Dialogs.RootDialog());
+                var connectorClient = new ConnectorClient(new Uri(activity.ServiceUrl));
+                var reply = activity.CreateReply(MicrosoftBotFrameworkFaq.FindAnAnswer(activity.Text));
+                await connectorClient.Conversations.ReplyToActivityAsync(reply);
             }
-            else
-            {
-                HandleSystemMessage(activity);
-            }
+
             var response = Request.CreateResponse(HttpStatusCode.OK);
             return response;
-        }
-
-        private Activity HandleSystemMessage(Activity message)
-        {
-            if (message.Type == ActivityTypes.DeleteUserData)
-            {
-                // Implement user deletion here
-                // If we handle user deletion, return a real message
-            }
-            else if (message.Type == ActivityTypes.ConversationUpdate)
-            {
-                // Handle conversation state changes, like members being added and removed
-                // Use Activity.MembersAdded and Activity.MembersRemoved and Activity.Action for info
-                // Not available in all channels
-            }
-            else if (message.Type == ActivityTypes.ContactRelationUpdate)
-            {
-                // Handle add/remove from contact lists
-                // Activity.From + Activity.Action represent what happened
-            }
-            else if (message.Type == ActivityTypes.Typing)
-            {
-                // Handle knowing tha the user is typing
-            }
-            else if (message.Type == ActivityTypes.Ping)
-            {
-            }
-
-            return null;
         }
     }
 }
