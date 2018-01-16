5141bd508ad6a12112e3d469362fab105050257f
Michael Roberts <michael.roberts@aldautomotive.com>
2018-01-16 15:10:10 +0000

    Adding options dialog and used in bot implementation to ask user about Weather, Bots or something else.

--- WeatherBot/Dialogs/DialogRootBuilder.cs
+++ WeatherBot/Dialogs/DialogRootBuilder.cs
@@ -7,23 +7,37 @@ namespace WeatherBot.Dialogs
     [Serializable]
     public static class DialogRootBuilder
     {
+        private const string Weather = "Weather";
+        private const string Bots = "Bots";
+        private const string SomethingElse = "Something else";
+
         public static IDialog<object> BuildRootDialog()
         {
+            var options = new string[] { Weather, Bots, SomethingElse };
+
             return
-                (from boolean in new YesNoDialog() select boolean).Switch(
-                        new Case<bool?, IDialog<object>>(x => x.HasValue && x.Value, (_, __) =>
-                            from questionToUser in new DisplayStringDialog("What do you want to know?")
-                            from intent in new WitIntentValueDialog("E6DFDKGS4STETYSJ7DVS6YASQFZNKTPX")
-                            from answer in new MsBotFaqAnswerDialog(intent)
-                            from result in new DisplayStringDialog(answer)
-                            select result),
-                        new Case<bool?, IDialog<object>>(x => x.HasValue && !x.Value, (_, __) => 
-                            from result in new DisplayStringDialog("I'm sorry to hear that.")
-                            select result),
-                        new DefaultCase<bool?, IDialog<object>>((_, __) => 
-                            from result in new DisplayStringDialog("What was that!?")
-                            select result)).Unwrap();
-;
+                (from option in new OptionsDialog("What are you interested in?", options) select option).Switch(
+                    // If the user chose Bots
+                    new Case<string, IDialog<object>>(x => x.Equals(Bots), (_, __) =>
+                        from questionToUser in new DisplayStringDialog("What do you want to know about Bots?")
+                        from intent in new WitIntentValueDialog("E6DFDKGS4STETYSJ7DVS6YASQFZNKTPX")
+                        from answer in new MsBotFaqAnswerDialog(intent)
+                        from result in new DisplayStringDialog(answer)
+                        select result),
+                     // If the user chose Weather
+                     new Case<string, IDialog<object>>(x => x.Equals(Weather), (_, __) =>
+                        from result in new DisplayStringDialog("Weather => Coming to a town near you soon!")
+                        select result),
+
+                     // If the user chose something else
+                     new Case<string, IDialog<object>>(x => x.Equals(SomethingElse), (_, __) =>
+                        from result in new DisplayStringDialog("I'm sorry to hear that.....What's wrong with Weather or Bots!")
+                        select result),
+
+                    // Invalid response
+                    new DefaultCase<string, IDialog<object>>((_, __) =>
+                        from result in new DisplayStringDialog("What was that!?")
+                        select result)).Unwrap();
         }
     }
 }
---
+++ WeatherBot/Dialogs/OptionsDialog.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.Bot.Builder.Dialogs;
+using Microsoft.Bot.Connector;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace WeatherBot.Dialogs
+{
+    [Serializable]
+    public class OptionsDialog : IDialog<string>
+    {
+        private readonly string _messageText;
+        private readonly IEnumerable<string> _options;
+
+        public OptionsDialog(string messageText, IEnumerable<string> options)
+        {
+            _messageText = messageText;
+            _options = options;
+        }
+
+        public async Task StartAsync(IDialogContext context)
+        {
+            var message = context.MakeMessage();
+            message.Text = _messageText;
+            message.SuggestedActions = new SuggestedActions
+            {
+                Actions = _options.Select(option => new CardAction
+                {
+                    Title = option,
+                    Type = ActionTypes.PostBack,
+                    Value = option
+                }).ToList()
+            };
+
+            await context.PostAsync(message);
+            context.Wait(MessageReceivedAsync);
+        }
+
+        private async Task MessageReceivedAsync(IDialogContext context, IAwaitable<IMessageActivity> result)
+        {
+            var activity = await result;
+            var matchedOption = _options.FirstOrDefault(o => o.Equals(activity.Text, StringComparison.InvariantCultureIgnoreCase));
+            if (matchedOption != null)
+            {
+                context.Done<string>(matchedOption);
+            }
+            else
+            {
+                context.Done<string>(null);
+            }
+        }
+    }
+}
